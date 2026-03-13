77f14bc8927851390b640c82df2cbd08e9c459db
Roberto German Guedes Neto <rgermangn@gmail.com>
2026-03-13 17:44:30 -0300

    feat: Implementa o módulo com função de busca no arquivo scanner.rs, então o teste foi movido para o mesmo arquivo. Passou no teste.
    Signed-off-by: Roberto German Guedes Neto <rgermangn@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,11 @@
-fn main() {}
+mod scanner;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::{
-        fs::{self, File},
-        path::PathBuf,
-    };
-    use tempfile::{TempDir, tempdir};
+use std::path::PathBuf;
 
-    #[test]
-    fn test_find_png_files_recursively() {
-        // Cria um diretório temporário
-        let dir: TempDir = tempdir().unwrap();
-        let sub_dir: PathBuf = dir.path().join("sub");
-        fs::create_dir(&sub_dir).unwrap();
+fn main() {
+    let path = PathBuf::from("./assets");
 
-        // Cria arquivos de teste
-        File::create(dir.path().join("foto1.png")).unwrap();
-        File::create(sub_dir.join("foto2.png")).unwrap();
-        File::create(dir.path().join("texto.txt")).unwrap();
+    let files = scanner::find_png_files(path);
 
-        // Chama a função de busca
-        let found_files = find_png_files(dir.path().to_path_buf());
-
-        // Validação
-        assert_eq!(found_files.len(), 2);
-    }
+    println!("Arquivos encontrados: {:?}", files);
 }
