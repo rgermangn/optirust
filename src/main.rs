111037fc3b10e15d39e964f0494a0b6c07c7a4f8
Roberto German Guedes Neto <rgermangn@gmail.com>
2026-03-13 08:05:54 -0300

    test: Adiciona o teste para o buscador PNG
    Signed-off-by: Roberto German Guedes Neto <rgermangn@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,30 @@
 fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        fs::{self, File},
+        path::PathBuf,
+    };
+    use tempfile::{TempDir, tempdir};
+
+    #[test]
+    fn test_find_png_files_recursively() {
+        // Cria um diretório temporário
+        let dir: TempDir = tempdir().unwrap();
+        let sub_dir: PathBuf = dir.path().join("sub");
+        fs::create_dir(&sub_dir).unwrap();
+
+        // Cria arquivos de teste
+        File::create(dir.path().join("foto1.png")).unwrap();
+        File::create(sub_dir.join("foto2.png")).unwrap();
+        File::create(dir.path().join("texto.txt")).unwrap();
+
+        // Chama a função de busca
+        let found_files = find_png_files(dir.path().to_path_buf());
+
+        // Validação
+        assert_eq!(found_files.len(), 2);
+    }
+}
